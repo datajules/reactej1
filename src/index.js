2694dd7f0883977eab66054c83de99625b36dc9c
Julio Vicente Guadarrama Olvera <42619125+datajules@users.noreply.github.com>
2021-08-10 18:01:55 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+ReactDOM.render(
+  <div>
+    <h1>Alimentos:</h1>
+    <ul>
+      <li>Amburguesa</li>
+      <li>Fruta</li>
+      <li>Amburguesa</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
